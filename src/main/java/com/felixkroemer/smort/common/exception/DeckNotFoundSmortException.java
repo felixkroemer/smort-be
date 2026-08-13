7399de07c36289b26dc12b34f333b4b78a21d3b7
Felix Krömer <kroemer.felix@gmail.com>
2026-08-14 00:40:06 +0200

    revert: drop tests and concrete deck exceptions, keep NotFoundSmortException

--- src/main/java/com/felixkroemer/smort/common/exception/DeckNotFoundSmortException.java
+++
@@ -1,8 +0,0 @@
-package com.felixkroemer.smort.common.exception;
-
-public class DeckNotFoundSmortException extends NotFoundSmortException {
-
-  public DeckNotFoundSmortException(Long id) {
-    super("Deck not found: " + id);
-  }
-}
