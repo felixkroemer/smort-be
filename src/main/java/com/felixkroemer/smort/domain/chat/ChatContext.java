0bdaf85750a97a1014621bc75222ebc9182a77d1
Felix Krömer <kroemer.felix@gmail.com>
2026-08-18 01:33:17 +0200

    Revert "feat: add ChatContext sealed interface with NoteChatContext and DeckChatContext"
    This reverts commit 005eac3409dffcf235e9ed029f49200ded34e0f7.

--- src/main/java/com/felixkroemer/smort/domain/chat/ChatContext.java
+++
@@ -1,3 +0,0 @@
-package com.felixkroemer.smort.domain.chat;
-
-public sealed interface ChatContext permits NoteChatContext, DeckChatContext {}
--- src/main/java/com/felixkroemer/smort/domain/chat/DeckChatContext.java
+++
@@ -1,5 +0,0 @@
-package com.felixkroemer.smort.domain.chat;
-
-import java.util.UUID;
-
-public record DeckChatContext(UUID deckId, String deckName) implements ChatContext {}
