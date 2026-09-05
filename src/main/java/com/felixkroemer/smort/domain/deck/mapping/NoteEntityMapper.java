36c8ff373c4d087acf6e4034af81b2ef16e134da
Felix Krömer <kroemer.felix@gmail.com>
2026-09-05 16:11:55 +0200

    feat: add userId to NoteEntityMapper mapping

--- src/main/java/com/felixkroemer/smort/domain/deck/mapping/NoteEntityMapper.java
+++ src/main/java/com/felixkroemer/smort/domain/deck/mapping/NoteEntityMapper.java
@@ -25,7 +25,10 @@ public interface NoteEntityMapper {
   @Mapping(target = "content", ignore = true)
   @Mapping(target = "pk", source = "deckId", qualifiedByName = "notePk")
   @Mapping(target = "sk", source = "noteId", qualifiedByName = "noteSk")
-  NoteEntity toNoteEntity(UUID deckId, UUID noteId, NoteSchema noteSchema);
+  @Mapping(target = "userId", source = "userId")
+  @Mapping(target = "userNoteIndexGsiPk", source = "userId", qualifiedByName = "userNoteIndexGsiPk")
+  @Mapping(target = "userNoteIndexGsiSk", source = "noteId", qualifiedByName = "userNoteIndexGsiSk")
+  NoteEntity toNoteEntity(UUID deckId, UUID noteId, NoteSchema noteSchema, String userId);
 
   @Named("notePk")
   default String toNotePk(UUID deckId) {
@@ -34,6 +37,16 @@ public interface NoteEntityMapper {
 
   @Named("noteSk")
   default String toNoteSk(UUID noteId) {
+    return NoteKeys.noteSk(noteId);
+  }
+
+  @Named("userNoteIndexGsiPk")
+  default String toUserNoteIndexGsiPk(String userId) {
+    return NoteKeys.userNoteIndexGsiPk(userId);
+  }
+
+  @Named("userNoteIndexGsiSk")
+  default String toUserNoteIndexGsiSk(UUID noteId) {
     return NoteKeys.noteSk(noteId);
   }
 
