63d0d71871dd68847e0817211043bf150bc3494f
Felix Krömer <kroemer.felix@gmail.com>
2026-09-13 12:08:01 +0200

    feat(vpc): add internet gateway and NAT routing

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -43,3 +43,34 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, { Name = var.name })
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = merge(var.tags, { Name = "${var.name}-nat" })
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = merge(var.tags, { Name = "${var.name}-nat" })
+}
+
+resource "aws_route" "private_internet" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
+}
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -17,3 +17,7 @@ output "public_route_table_id" {
 output "private_route_table_id" {
   value = aws_route_table.private.id
 }
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.this.id
+}
