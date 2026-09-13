74d4e127328e5e11cb5c98e95350dd9d076a855f
Felix Krömer <kroemer.felix@gmail.com>
2026-09-13 13:01:24 +0200

    chore(vpc): include resource type in Name tags

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "this" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = { Name = var.name }
+  tags = { Name = "${var.name}-vpc" }
 }
 
 resource "aws_subnet" "public" {
@@ -11,7 +11,7 @@ resource "aws_subnet" "public" {
   cidr_block        = "10.0.0.0/24"
   availability_zone = var.az
 
-  tags = { Name = "${var.name}-public" }
+  tags = { Name = "${var.name}-subnet-public" }
 }
 
 resource "aws_subnet" "private" {
@@ -19,19 +19,19 @@ resource "aws_subnet" "private" {
   cidr_block        = "10.0.1.0/24"
   availability_zone = var.az
 
-  tags = { Name = "${var.name}-private" }
+  tags = { Name = "${var.name}-subnet-private" }
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
 
-  tags = { Name = "${var.name}-public" }
+  tags = { Name = "${var.name}-route-table-public" }
 }
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
-  tags = { Name = "${var.name}-private" }
+  tags = { Name = "${var.name}-route-table-private" }
 }
 
 resource "aws_route_table_association" "public" {
@@ -47,7 +47,7 @@ resource "aws_route_table_association" "private" {
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
-  tags = { Name = var.name }
+  tags = { Name = "${var.name}-igw" }
 }
 
 resource "aws_route" "public_internet" {
@@ -59,14 +59,14 @@ resource "aws_route" "public_internet" {
 resource "aws_eip" "nat" {
   domain = "vpc"
 
-  tags = { Name = "${var.name}-nat" }
+  tags = { Name = "${var.name}-eip-nat" }
 }
 
 resource "aws_nat_gateway" "this" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public.id
 
-  tags = { Name = "${var.name}-nat" }
+  tags = { Name = "${var.name}-nat-gateway" }
 }
 
 resource "aws_route" "private_internet" {
@@ -85,7 +85,7 @@ resource "aws_vpc_endpoint" "dynamodb" {
   service_name      = data.aws_vpc_endpoint_service.dynamodb.service_name
   vpc_endpoint_type = "Gateway"
 
-  tags = { Name = "${var.name}-dynamodb" }
+  tags = { Name = "${var.name}-vpc-endpoint-dynamodb" }
 }
 
 resource "aws_vpc_endpoint_route_table_association" "dynamodb" {
