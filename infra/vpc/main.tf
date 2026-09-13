ab136896868e86bf542ad99ccc8d30c97c3be098
Felix Krömer <kroemer.felix@gmail.com>
2026-09-13 12:17:57 +0200

    feat(vpc): add root caller with provider and outputs

---
+++ infra/vpc/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  name   = var.name
+  region = var.region
+  az     = var.az
+}
---
+++ infra/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  type    = string
+  default = "smort"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "az" {
+  type    = string
+  default = "eu-central-1a"
+}
